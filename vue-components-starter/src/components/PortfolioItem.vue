982a114578e0719916eb0f89cd723afeac7e49e2
l-huisman <huisman.l@yahoo.com>
2023-02-17 09:15:42 +0100

    Added PortfolioItem

---
+++ vue-components-starter/src/components/PortfolioItem.vue
@@ -0,0 +1,43 @@
+<template><!-- Individual stock in portfolio -->
+    <div class="col-md-4 mt-4">
+        <div class="card">
+            <div class="card-body">
+                {{ stock.name }}
+                <span :class="{
+                    down: stock.price < currentPrice,
+                    up: stock.price > currentPrice,
+                }">{{ stock.currency }} {{ stock.price.toFixed(4) }} </span>
+            </div>
+            <div class="card-footer">
+                <span>Amount: {{ stock.amount }}</span>
+                {{ showMeStuff() }}
+            </div>
+        </div>
+    </div>
+<!-- End of individual stock in portfolio --></template>
+
+<script>
+export default {
+    name: "PortfolioItem",
+    props: {
+        stock: Object,
+        currentPrice: Number
+    },
+    methods: {
+        showMeStuff() {
+            console.log(this.stock);
+            console.log(this.currentPrice);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.up {
+    color: green;
+}
+
+.down {
+    color: red;
+}
+</style>
